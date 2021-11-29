db0a858c248c556b34297951518463ee0596fbe6
Sapa <mosevaly@gmail.com>
2021-11-29 21:01:53 +0300

    Initial Commit

---
+++ HomeWork2 (sliders)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork2 (sliders)
+//
+//  Created by Sergey Pavlov on 29.11.2021.
+//  Copyright © 2021 Sergey Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
